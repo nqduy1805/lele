df8a302ba095ae0690aaa7aa7fc33614f487a46f
Quoc Duy <duy.nguyen@southtelecom.vn>
2024-08-12 12:02:07 +0700

    u

--- app/ui/invoices/table.tsx
+++ app/ui/invoices/table.tsx
@@ -1,6 +1,5 @@
 import Image from 'next/image';
-import { UpdateInvoice, DeleteInvoice } from '@/app/ui/invoices/buttons';
-import InvoiceStatus from '@/app/ui/invoices/status';
+
 import { formatDateToLocal, formatCurrency } from '@/app/lib/utils';
 import { fetchFilteredInvoices } from '@/app/lib/data';
 
@@ -37,7 +36,6 @@ export default async function InvoicesTable({
                     </div>
                     <p className="text-sm text-gray-500">{invoice.email}</p>
                   </div>
-                  <InvoiceStatus status={invoice.status} />
                 </div>
                 <div className="flex w-full items-center justify-between pt-4">
                   <div>
@@ -47,8 +45,7 @@ export default async function InvoicesTable({
                     <p>{formatDateToLocal(invoice.date)}</p>
                   </div>
                   <div className="flex justify-end gap-2">
-                    <UpdateInvoice id={invoice.id} />
-                    <DeleteInvoice id={invoice.id} />
+                    
                   </div>
                 </div>
               </div>
@@ -105,12 +102,10 @@ export default async function InvoicesTable({
                     {formatDateToLocal(invoice.date)}
                   </td>
                   <td className="whitespace-nowrap px-3 py-3">
-                    <InvoiceStatus status={invoice.status} />
                   </td>
                   <td className="whitespace-nowrap py-3 pl-6 pr-3">
                     <div className="flex justify-end gap-3">
-                      <UpdateInvoice id={invoice.id} />
-                      <DeleteInvoice id={invoice.id} />
+                      
                     </div>
                   </td>
                 </tr>
--- app/ui/layout/navbar/hero.tsx
+++ app/ui/layout/navbar/hero.tsx
@@ -6,7 +6,7 @@ export default function Hero() {
     <div className="bg-beige flex flex-col justify-center lg:justify-end items-center w-full mb-[3vw] relative min-h-[40vh] md:min-h-[60vh] ">
         <div className="relative flex flex-col justify-center items-start max-w-none z-10 ml-auto mr-auto">
             <div className="relative flex flex-col justify-center items-start w-full  pl-[3vw] pr-[3vw] pt-[0] lg:pt-[3vw] top-[0px] lg:top-[30px]">
-                <h1 className={`${styles.animateFromLeft} animate-slide-in z-5 text-light-onyx-gray relative text-center mt-5 mb-0 text-[6rem] md:text-[8rem] lg:text-[15rem] font-bold leading-none w-full`}>vitale</h1>
+                <h1 className={`${styles.animateFromLeft} animate-slide-in z-5 text-light-onyx-gray relative text-center mt-5 mb-0 text-[6rem] md:text-[8rem] lg:text-[15rem] font-bold leading-none w-full`}>LeLe shop</h1>
                 <div className={`${styles.animateFromRight} font-thin  bg-mango-yellow text-light-onyx-gray min-h-[80px] text-center w-full relative pr-[30px] pl-[30px] pt-[30px] pb-[20px] mt-[-20px] m-h-[80px] text-xl md:text-[2rem] leading-[150%]`}>organic food you can trust.</div>
             </div>
         </div>
