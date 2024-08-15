85526c7383201a1d946d9a36879309be6ab4a2f1
Quoc Duy <duy.nguyen@southtelecom.vn>
2024-08-15 16:16:49 +0700

    y

--- next.config.mjs
+++ next.config.mjs
@@ -4,6 +4,15 @@ const nextConfig = {
     experimental: {
       ppr: 'incremental',
     },
+    async redirects() {
+      return [
+        {
+          source: '/',
+          destination: '/home',
+          permanent: true,
+        },
+      ]
+    },
   };
 
 export default nextConfig;
