ff98abc1a740f0a59dcf972e0f7abf9e4133d5e5
Quoc Duy <duy.nguyen@southtelecom.vn>
2024-09-26 17:19:27 +0700

    UPDATE 26/9

--- app/api/cart/route.ts
+++ app/api/cart/route.ts
@@ -9,9 +9,8 @@ export const POST = handler(async (request: requestCustom) =>{
     const data = await request.json();
     const {id:user_id} = request.authen;
     const {product_id, quantity} = data;
-
     await sql.query(
-        'INSERT INTO carts (user_id, product_id, quantity) VALUES ($1, $2, $3)',
+        'INSERT INTO carts (user_id, product_id, quantity) VALUES ($1, $2, $3) ON CONFLICT (user_id,product_id)  DO UPDATE SET quantity = carts.quantity + EXCLUDED.quantity',
         [user_id, product_id, quantity]
     );
     return NextResponse.json( { status: 200 });
